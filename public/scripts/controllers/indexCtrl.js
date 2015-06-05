b986e9c5e7d9fd818cffbe04e26916f006305ad1
David Wieczorek <David.Wieczorek@DWK-WIN.ercom-net.local>
2015-06-05 17:16:02 +0200

    Adding ScrollFit functionality ctrl and css

--- public/scripts/controllers/indexCtrl.js
+++ public/scripts/controllers/indexCtrl.js
@@ -3,7 +3,21 @@ function ($scope, $rootScope, $http) {
 
 }]);
 
-controllers.controller('componentsCtrl', ['$scope', '$rootScope', '$http',
-function ($scope, $rootScope, $http) {
+controllers.controller('componentsCtrl', ['$scope', '$rootScope', '$http', '$timeout',
+function ($scope, $rootScope, $http, $timeout) {
+
+   $timeout(function() {
+      $(function(){
+         $(window).scroll(function() {
+           if ($(this).scrollTop() >= 30) {
+               $('.fixThis').addClass('stickytop');
+           }
+           else if ($(this).scrollTop() <= 100) {
+               $('.fixThis').removeClass('stickytop');
+           }
+         });
+      });
+   }, 100);
    console.log('components controller');
+
 }]);
