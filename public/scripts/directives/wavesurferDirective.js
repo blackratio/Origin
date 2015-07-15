6f83b2a07cf777cc7a2d4282a02cde1bd7654f90
blackratio <blackratio@gmail.com>
2015-07-16 01:45:57 +0200

    minor directive change

--- public/scripts/directives/wavesurferDirective.js
+++ public/scripts/directives/wavesurferDirective.js
@@ -1,19 +1,8 @@
 directives.directive('waveSurfer', function() {
-   return {
-      restrict: 'E',
-      templateUrl: 'partials/directives/wavesurfer.html',
-      link: link
-   };
 
    function link( scope, element, attributes, controller ) {
 
       var datas = scope.list.data;
-
-      datas.forEach(function(data){
-         if(data.content) {
-            console.log(data);
-         }
-      });
 
       var wavesurfer = Object.create(WaveSurfer);
       wavesurfer.init({
@@ -22,15 +11,20 @@ directives.directive('waveSurfer', function() {
          progressColor: 'purple',
          splitChannels: true
       });
-      /*wavesurfer.on('ready', function () {
-      wavesurfer.play();
-      });*/
-      // Play/pause on button press
+
+      // Play/pause
       document.querySelector('[data-action="play"]').addEventListener(
          'click', wavesurfer.playPause.bind(wavesurfer)
       );
+
       wavesurfer.load(datas[2].content.file);
 
    }
+
+   return {
+      restrict: 'E',
+      templateUrl: 'partials/directives/wavesurfer.html',
+      link: link
+   };
 
 });
