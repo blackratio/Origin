271a2f03799a86ad2fe8b8d0e5c308fd3b26b7f9
blackratio <blackratio@gmail.com>
2015-09-15 21:05:18 +0200

    adding firebase

--- public/scripts/controllers/indexCtrl.js
+++ public/scripts/controllers/indexCtrl.js
@@ -1,10 +1,22 @@
+<<<<<<< Updated upstream
 controllers.controller('homeCtrl', ['$scope', '$rootScope', '$http', '$timeout',
 function ($scope, $rootScope, $http, $timeout) {
+=======
+controllers.controller('homeCtrl', ['$scope', '$rootScope', '$http', '$firebaseObject',
+function ($scope, $rootScope, $http, $firebaseObject) {
+>>>>>>> Stashed changes
 
    $scope.name = "Daphné";
    $scope.sayHello = function() {
       $scope.greeting = "Hello " + $scope.name;
    };
+
+   var ref = new Firebase("https://blackratiobase.firebaseio.com/data");
+   var syncObject = $firebaseObject(ref);
+   console.log($firebaseObject(ref));
+   // synchronize the object with a three-way data binding
+   // click on `index.html` above to see it used in the DOM!
+   syncObject.$bindTo($scope, "infos");
 
 }]);
 
